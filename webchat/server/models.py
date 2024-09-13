f490dee5450387e68aafa804d9a460674e04f0ad
Dmytró Voronkov <dmvr2107@gmail.com>
2024-09-13 20:07:58 +0300

    Created Server Model Classes

--- webchat/server/models.py
+++ webchat/server/models.py
@@ -1,3 +1,44 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def _str__(self):
+        return self.name
+
+
+class Server(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="server_owner"
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="server_category"
+    )
+    description = models.CharField(max_length=250, null=True)
+    member = models.ManyToManyField(settings.AUTH_USER_MODEL)
+
+    def _str__(self):
+        return self.name
+
+
+class Channel(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="channel_owner"
+    )
+    topic = models.CharField(max_length=100)
+
+    server = models.ForeignKey(
+        Server, on_delete=models.CASCADE, related_name="channels_server"
+    )
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.lower()
+        return super(Channel, self).save()
+
+    def _str__(self):
+        return self.name
