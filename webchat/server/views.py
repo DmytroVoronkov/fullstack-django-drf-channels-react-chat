a2a6d223df851950173158008c15650b66f5ad31
Dmytró Voronkov <dmvr2107@gmail.com>
2024-09-20 18:20:50 +0300

    Added server filter by server id

--- webchat/server/views.py
+++ webchat/server/views.py
@@ -1,5 +1,6 @@
 # from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.exceptions import ValidationError, AuthenticationFailed
 from rest_framework.request import Request
 from rest_framework.response import Response
 
@@ -13,17 +14,31 @@ class ServerListViewSet(viewsets.ViewSet):
     def list(self, request: Request) -> Response:
         category = request.query_params.get("category")
         qty = request.query_params.get("qty")
-        by_user = request.query_params.get("by_user") == "true"
+        by_user = request.query_params.get("by_user")
+        by_server_id = request.query_params.get("by_server_id")
+
+        if (by_user or by_server_id) and not request.user.is_authenticated:
+            raise AuthenticationFailed("You are not authenticated")
 
         if category:
             self.queryset = self.queryset.filter(category__name=category)
 
-        if by_user:
+        if by_user and by_user.lower() == "true":
             user_id = request.user.id
             self.queryset = self.queryset.filter(member=user_id)
 
         if qty:
             self.queryset = self.queryset[: int(qty)]
+
+        if by_server_id:
+            try:
+                self.queryset = self.queryset.filter(id=by_server_id)
+                if not self.queryset.exists():
+                    raise ValidationError(
+                        detail=f"Server with id {by_server_id} does not exist"
+                    )
+            except ValueError:
+                raise ValidationError(detail="Server value error")
 
         serializer = ServerSerializer(self.queryset, many=True)
         return Response(serializer.data)
