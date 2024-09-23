56c5e13f12e3c3c11f1b37166bb90a3888e11db7
Dmytró Voronkov <dmvr2107@gmail.com>
2024-09-23 17:31:09 +0300

    Added File Field in Channel Model

--- webchat/server/models.py
+++ webchat/server/models.py
@@ -1,13 +1,35 @@
 from django.conf import settings
 from django.db import models
+from django.dispatch import receiver
+from django.shortcuts import get_object_or_404
+
+
+def category_upload_path(instance: models.Model, filename: str) -> str:
+    return f"category/{instance.id}/category_icon/{filename}"
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
+    icon = models.FileField(null=True, blank=True, upload_to=category_upload_path)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if self.id:  # Check if object already exists
+            existing = get_object_or_404(Category, id=self.id)
+            if existing.icon != self.icon:
+                existing.icon.delete(save=False)
+        return super(Category, self).save(*args, **kwargs)
+
+    @receiver(models.signals.pre_delete, sender="server.Category")
+    def category_delete_files(sender, instance, **kwargs):
+        for field in instance._meta.fields:
+            if field.name == "icon":
+                file = getattr(instance, field.name)
+                if file:
+                    file.delete(save=False)
 
     class Meta:
         verbose_name_plural = "Categories"
