ae92266e3fa0e23b614d0a3395542ac3e636d471
Dmytró Voronkov <dmvr2107@gmail.com>
2024-10-20 11:45:41 +0300

    Fixed ProtectedRoute

--- frontend/src/services/ProtectedRoute.tsx
+++ frontend/src/services/ProtectedRoute.tsx
@@ -5,11 +5,7 @@ import { Navigate } from "react-router-dom";
 const ProtectedRoute: React.FC<React.PropsWithChildren> = ({ children }) => {
   const { isLoggedIn } = useAuthServiceContext();
 
-  // FIXME: Create helper
-  const loggedFromStorage = localStorage.getItem("isLoggedIn") === "true"
-  const isAuth = isLoggedIn ? isLoggedIn : loggedFromStorage;
-
-  return isAuth ? <div>{children}</div> : <Navigate to="/login" replace={true} />;
+  return isLoggedIn ? <div>{children}</div> : <Navigate to="/login" replace={true} />;
 };
 
 export default ProtectedRoute;
